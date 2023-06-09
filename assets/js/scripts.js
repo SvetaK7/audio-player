b38bf5deafe7d96feeb8fec44d1a583d6cbd7efa
SvetaK7 <atevs7@inbox.ru>
2023-06-09 23:37:06 +0300

    add loadAudioData

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,6 +15,28 @@ const AudioController = {
     initVariables(){
         this.audioList = document.querySelector('.items')
     },
+
+    loadAudioData(audio){
+        const {id, link, genre, track, group, duration} = audio;
+        const [image] = link.split('.');
+        console.log([image]);
+
+        const item = ` <div class="item" data-id = ${id}>
+                        <div class="item-image" style="background-image: url(./assets/images/${image}.jpg)"></div>
+                        <div class="item-titles">
+                            <h2 class="item-group">${group}</h2>
+                            <h3 class="item-track">${track}</h3>
+                        </div>
+                        <p class="item-duration">${duration}</p>
+                        <p class="item-genre">${genre}</p>
+                        <button class="item-play">
+                            <svg class="icon-play">
+                                <use xlink:href="./assets/images/sprite.svg#play"></use>
+                            </svg>
+                        </button>
+                    </div>`
+    },
+
     renderAudios(){
         data.forEach((item)=>{
             const audio = new Audio(`./assets/audio/${item.link}`)
@@ -22,7 +44,7 @@ const AudioController = {
                 const newItem = {...item, duration: audio.duration, audio}
                 // this.state.audios = {...this.state, newItem};
                 this.state.audios.push(newItem)
-                console.log(this.state.audios);
+                this.loadAudioData(newItem)
             })
            
         })
